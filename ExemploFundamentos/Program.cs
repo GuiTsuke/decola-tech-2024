80b8031166c891474834282938d9710606fe12a7
geovanna0101 <geh12_navarro@hotmail.com>
2023-12-29 09:15:04 -0300

    #4 - Combinando Operadores

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -1,23 +1,41 @@
 ﻿using ExemploFundamentos.Models;
 
+int a = 10;
+int b = 20; 
 
-DateTime  dataAtual = DateTime.Now.AddDays(5);
+int c = a + b;
 
-Console.WriteLine(dataAtual.ToString("dd/mm/yyyy"));
+// c = c + 5; 
+// combinação de operadores: +=, -=, /=, *= 
+
+c += 5;
+
+
+Console.WriteLine(c);
 
 
 
-string apresentacao = "Olá, seja bem-vindo!";
-int quantidade = 1;
-double altura = 1.80;
-decimal preco = 1.80M;
-bool condicao = true;
 
+
+
+
+// DateTime  dataAtual = DateTime.Now.AddDays(5);
+// Console.WriteLine(dataAtual.ToString("dd/mm/yyyy"));
+
+
+
+// string apresentacao = "Olá, seja bem-vindo!";
+// int quantidade = 1;
+// double altura = 1.80;
+// decimal preco = 1.80M;
+// bool condicao = true;
 // Console.WriteLine(apresentacao);
 // Console.WriteLine("Valor da variavel quantidade " + quantidade);
 // Console.WriteLine("Valor da variavel altura " + altura);
 // Console.WriteLine("Valor da variavel preço " + preco);
 // Console.WriteLine("Valor da variavel condição " + condicao);
+
+
 
 
 // // Pessoa pessoa1 = new Pessoa();
