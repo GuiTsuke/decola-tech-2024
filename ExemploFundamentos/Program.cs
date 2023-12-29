8aed621a1588c128ecc5884bdeb010bbbcbdc375
geovanna0101 <geh12_navarro@hotmail.com>
2023-12-28 21:05:23 -0300

    Teste de commit colaborador

---
+++ ExemploFundamentos/Program.cs
@@ -0,0 +1,26 @@
+﻿using ExemploFundamentos.Models;
+
+
+DateTime  dataAtual = DateTime.Now.AddDays(5);
+
+Console.WriteLine(dataAtual.ToString("dd/mm/yyyy"));
+
+
+
+// string apresentacao = "Olá, seja bem-vindo!";
+// int quantidade = 1;
+// double altura = 1.80;
+// decimal preco = 1.80M;
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variavel quantidade " + quantidade);
+// Console.WriteLine("Valor da variavel altura " + altura);
+// Console.WriteLine("Valor da variavel preço " + preco);
+// Console.WriteLine("Valor da variavel condição " + condicao);
+
+
+// // Pessoa pessoa1 = new Pessoa();
+// // pessoa1.Nome = "Geovanna";
+// // pessoa1.Idade = 21;
+// // pe ssoa1.Apresentar();
