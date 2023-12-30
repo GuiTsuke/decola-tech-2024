bdb35da8e343d4b07ce3cd54261480fbbeb9a0b4
geovanna0101 <geh12_navarro@hotmail.com>
2023-12-29 21:45:46 -0300

    #4 - Incremento e Decremento

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -1,17 +1,49 @@
 ﻿using ExemploFundamentos.Models;
 
 
-//Classe calculadora utilizando Seno, Coseno e Tangente
-Calculadora calc = new Calculadora();
+//Incremento e Decremento 
 
-// // calc.Somar(7, 30);
-// // calc.Subtrair(10, 50);
-// // calc.Multiplicar(15,45);
-// // calc.Dividir(2, 2);
-// calc.Potencia(3, 3);
-calc.Seno(30);
-calc.Coseno(30);
-calc.Tangente(30);
+int numeroIncremento = 10;
+
+Console.WriteLine("Incremento ao numero 10");
+numeroIncremento++;
+
+Console.WriteLine(numeroIncremento);
+
+
+int numeroDecremento = 10;
+
+Console.WriteLine("Decremento ao numero 10");
+numeroDecremento--;
+
+Console.WriteLine(numeroDecremento);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Classe calculadora utilizando Seno, Coseno e Tangente
+// Calculadora calc = new Calculadora();
+
+// // // calc.Somar(7, 30);
+// // // calc.Subtrair(10, 50);
+// // // calc.Multiplicar(15,45);
+// // // calc.Dividir(2, 2);
+// // calc.Potencia(3, 3);
+// calc.Seno(30);
+// calc.Coseno(30);
+// calc.Tangente(30);
 
 
 
